1d1c3d3bd079b5f1eaf489874669016ea51218d9
kazrias <ka6rias@gmail.com>
2024-02-26 21:56:23 +0400

    Add line indicator

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import './Header.scss'
 export const Header = ({ welcomeDone }) => {
+  const [line, setLine] = useState("about")
   return (
     <div className={`header ${welcomeDone ? 'animate' : ''}`}>
       <div className="header-menu">
-        <div className="header-menu__item">About</div>
-        <div className="header-menu__item">Project</div>
-        <div className="header-menu__item">Contact</div>
+        <div onClick={()=>setLine("about")} className="header-menu__item">About
+          <div className={`header-menu__line ${line}`}></div>
+        </div>
+        <div onClick={()=>setLine("projects")} className="header-menu__item">Projects</div>
+        <div onClick={()=>setLine("contact")} className="header-menu__item">Contact</div>
       </div>
     </div>
   )
